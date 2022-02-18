23c1983148db8a9763a273606b980032656d8905
dulimpul <duckg.diger46@gmail.com>
2022-02-19 01:40:38 +0800

    finish

--- main_aps.py
+++ main_aps.py
@@ -2,7 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
-from pdfminer import high_level
+
+#
+
+from nltk.tokenize import RegexpTokenizer
+from pdfminer.high_level import extract_text
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
 
 
 
@@ -39,6 +47,7 @@ class App(tk.Tk):
             
     # fungsi pilih file
     def pilih_file(self):
+        global linkse
 
         # file tipe fungsi
         self.file_tipe = (
@@ -63,8 +72,8 @@ class App(tk.Tk):
         )
         self.reks1.place(x=56, y=60)
         self.reks1.insert(tk.END, self.openfile,)
-
-        global linkse
+        
+        # mangambil link
         linkse = self.openfile
        
        
@@ -93,28 +102,26 @@ class Run_sektor(tk.Toplevel):    # sektor update nanti
         super().__init__()
         self.geometry("1000x700")
         prc = Procec()
-        self.reks1 = tk.Text(
-            self,
-            height=2,
-            width=35,
-            
-        )
-        self.reks1.place(x=56, y=60)
-        self.reks1.insert(tk.END,prc.extrack_hasil)
+       
+        #visual data
+        fig = plt.figure(figsize=(10,4))
+        plt.gcf().subplots_adjust(bottom=0.15)
+        fig.add_subplot(111).plot(prc.long_frekunc_pdf)
+        canvas = FigureCanvasTkAgg(fig, self)
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(fill=tk.BOTH, expand=10)
 
-
+        
 class Procec():
 
     def __init__(self):
-        lnk = App
-        path = linkse
-        pages = [0] # page 1
-        self.extrack_hasil = high_level.extract_text(path, "",pages)
-        
+        path_pdf = extract_text(linkse)
+        tokenz_pdf = RegexpTokenizer('\w+')
+        tokens_pdf = tokenz_pdf.tokenize(path_pdf)
+        frekunsi_pdf = FreqDist(tokens_pdf)
+        self.long_frekunc_pdf = [words for words in tokens_pdf if len(words) > 5 and frekunsi_pdf[words] > 20]
 
-class Visualisasi:   #< ---- sektor visual
-    pass
 
-    
 apps = App()
 apps.mainloop()
